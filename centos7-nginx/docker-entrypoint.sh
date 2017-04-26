490a807c10251e18d59707970b9b2cbf9be96f98
Valeriy Tropin <tropin.valeriy@gmail.com>
2017-04-26 20:57:28 +0300

    Allow to set user and group for php7.1 and nginx through env variables

---
+++ centos7-nginx/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ -z "${NGINX_USER}" ]]; then
+  NGINX_USER="www-data"
+else
+  NGINX_USER="${NGINX_USER}"
+fi
+
+if [[ -z "${NGINX_USER_GROUP}" ]]; then
+  NGINX_USER_GROUP="www-data"
+else
+  NGINX_USER_GROUP="${NGINX_USER_GROUP}"
+fi
+
+groupadd $NGINX_USER_GROUP
+useradd $NGINX_USER -g $NGINX_USER_GROUP
+
+sed -i -e "s/^user = .*/user = ${NGINX_USER};/g" /etc/nginx/nginx.conf
+
+nginx -g 'daemon off;'
---
+++ centos7-php7.1-remi/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ -z "${PHP_FPM_USER}" ]]; then
+  PHP_FPM_USER="www-data"
+else
+  PHP_FPM_USER="${PHP_FPM_USER}"
+fi
+
+if [[ -z "${PHP_FPM_GROUP}" ]]; then
+  PHP_FPM_GROUP="www-data"
+else
+  PHP_FPM_GROUP="${PHP_FPM_GROUP}"
+fi
+
+groupadd $PHP_FPM_GROUP
+useradd $PHP_FPM_USER -g $PHP_FPM_GROUP
+
+
+#TODO: change user in configs using sed
+sed -i -e "s/^user = .*/user = ${PHP_FPM_USER}/g" /etc/php-fpm.d/www.conf
+sed -i -e "s/^group = .*/user = ${PHP_FPM_GROUP}/g" /etc/php-fpm.d/www.conf
+
+php-fpm --nodaemonize
